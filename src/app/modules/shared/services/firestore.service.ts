875bf166174baf08104e0b2c0eacf57fbb99bb95
BrotherJT2610 <julianmoscon@gmail.com>
2024-09-09 21:10:37 -0300

    creación de función obtenerUID vinculado a la BD -> authentication/ firestore

--- src/app/modules/shared/services/firestore.service.ts
+++ src/app/modules/shared/services/firestore.service.ts
@@ -7,13 +7,37 @@ import { Usuario } from 'src/app/models/usuario';
   providedIn: 'root'
 })
 export class FirestoreService {
-    /**
-     * Definimos una colección de usuarios PRIVADA
-     * Va a ser una colección de Firestore
-     * Respetará la estructura de datos de la interfaz Usuario
-     */
-    private usuariosCollection: AngularFirestoreCollection<Usuario>
+  /**
+   * Definimos una colección de usuarios PRIVADA
+   * Va a ser una colección de Firestore
+   * Respetará la estructura de datos de la interfaz Usuario
+   */
+  private usuariosCollection: AngularFirestoreCollection<Usuario>
+
   constructor(private database: AngularFirestore) {
     this.usuariosCollection = this.database.collection<Usuario>('usuarios');
-   }
+  }
+
+  agregarUsuario(usuario: Usuario, id: string){
+    /* Generamos nueva PROMESA y utiliza los métodos:
+      RESOLVE: promesa resuelta -> funciona correctamente
+      REJECT: promesa rechaza -> ocurrió una falla
+    */
+    return new Promise(async (resolve, reject) => {
+      // Bloque TRY encapsula la lógica resuelta
+      try{
+        usuario.uid = id;
+
+        /**
+         * const resultado = colección de usuarios, envía como documento el UID 
+         * y setea la información que ingresemos en el REGISTRO
+         */
+        const resultado = await this.usuariosCollection.doc(id).set(usuario);
+        resolve(resultado);
+        // Bloque CATCH encapsula una falla y la vuelve un error
+      }catch(error){
+        reject (error);
+      }
+    })
+  }
 }
